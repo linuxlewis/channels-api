4814c62c574e717ec16ef1848f97567ddc3522d4
Sergey Pashinin <sergey@pashinin.com>
2018-01-11 15:25:21 +0300

    import IsSuperuser permission

--- channels_api/__init__.py
+++ channels_api/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.4.0"
 
 from .decorators import detail_action, list_action
-from .permissions import AllowAny, IsAuthenticated
+from .permissions import AllowAny, IsAuthenticated, IsSuperuser
